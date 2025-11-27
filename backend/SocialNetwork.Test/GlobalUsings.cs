8f70db2df970a6561130f56eabe129cffa26c0a9
emmamjs-7 <emmaamjs@gmail.com>
2025-11-27 12:56:47 +0100

    Created Global usings for test project

---
+++ backend/SocialNetwork.Test/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using System;
+global using System.Threading.Tasks;
+global using Moq;
+global using Xunit;
+
+global using SocialNetwork.Repository.Interfaces;
+global using SocialNetwork.Repository.Services;
+global using SocialNetwork.Entity;
