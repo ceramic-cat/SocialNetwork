1c996f282af76fe3d80f7a6b5e224e4cafebe574
emmamjs-7 <emmaamjs@gmail.com>
2025-11-28 12:11:13 +0100

    Created three tests for PostController and GlobalUsings

--- backend/SocialNetwork.Test/GlobalUsings.cs
+++ backend/SocialNetwork.Test/GlobalUsings.cs
@@ -2,7 +2,11 @@ global using System;
 global using System.Threading.Tasks;
 global using Moq;
 global using Xunit;
+global using Microsoft.AspNetCore.Mvc;
+
 
 global using SocialNetwork.Repository.Interfaces;
 global using SocialNetwork.Repository.Services;
 global using SocialNetwork.Entity;
+global using SocialNetwork.API.Controllers;
+global using SocialNetwork.API.Models;
