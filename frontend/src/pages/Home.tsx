2b772ef5448fb6d816f16a5461b24946fbb8e129
emmamjs-7 <emmaamjs@gmail.com>
2025-12-02 14:09:22 +0100

    Just for testing - create post is in homepage

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,13 @@
-
+import { useState } from "react";
+import CreatePostModal from "../components/CreatePostModal";
 
 export default function Home() {
+  const [show] = useState(true);
+
   return (
-    <div>
-      <h1>
-        Home
-      </h1>
+    <div className="text-center">
+      {/* Change to the correct senderId and senderName when we have users */}
+      <CreatePostModal senderId="user-123" senderName="Maja Berg" />
     </div>
-
-
-  )
+  );
 }
