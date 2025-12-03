3cf513b7244c4c73afac004c884f5b1a34e9ca49
emmamjs-7 <emmaamjs@gmail.com>
2025-12-03 10:43:49 +0100

    added an excisting senderId to test CreatePost. This will change later.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -7,7 +7,10 @@ export default function Home() {
   return (
     <div className="text-center">
       {/* Change to the correct senderId and senderName when we have users */}
-      <CreatePostModal senderId="user-123" senderName="Maja Berg" />
+      <CreatePostModal
+        senderId="405F3E28-E455-4F15-95E8-A890F54C5848"
+        senderName="Maja Berg"
+      />
     </div>
   );
 }
