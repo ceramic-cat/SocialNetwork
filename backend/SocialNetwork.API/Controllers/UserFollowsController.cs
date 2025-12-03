814d854fc27115416e7d4f92f3f926e7b4641342
ceramic-cat <120321392+ceramic-cat@users.noreply.github.com>
2025-12-03 15:00:11 +0100

    Created test for UserFollowsController
    Created test that fails when trying to follow another user.
    Created initial UserFollowsController so the project builds.

---
+++ backend/SocialNetwork.API/Controllers/UserFollowsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.Repository.Services;
+
+namespace SocialNetwork.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserFollowsController : ControllerBase
+{
+    private readonly IUserFollowsService _userFollowsService;
+
+    public UserFollowsController(IUserFollowsService userFollowsService)
+    {
+        _userFollowsService = userFollowsService;
+    }
+
+    [HttpPost("follow")]
+    public async Task<IActionResult> Follow()
+    {
+        throw new NotImplementedException();
+    }
+
+
+}
---
+++ backend/SocialNetwork.Test/Controllers/UserFollowsControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork.Test.Controllers;
+
+public class UserFollowsControllerTests
+{
+
+    private readonly Mock<IUserFollowsService> _followsUserServiceMock;
+    private readonly UserFollowsController _sut;
+    public UserFollowsControllerTests()
+    {
+        _followsUserServiceMock = new Mock<IUserFollowsService>();
+         _sut = new UserFollowsController( _followsUserServiceMock.Object );
+    }
+
+    [Fact]
+    public async Task FollowUser_ValidRequest_ReturnsOk()
+    {
+        //Arrange
+        _followsUserServiceMock.Setup( m => 
+        m.FollowAsync(It.IsAny<Guid>(), It.IsAny<Guid>())).ReturnsAsync(Result.Success);
+        var followerId = Guid.NewGuid();
+        var followeeId = Guid.NewGuid();
+
+        //Act
+
+        var reply = await _sut.Follow(followerId, followeeId);
+
+        // Assert
+        Assert.IsType<OkResult>(reply);
+        _followsUserServiceMock.Verify(
+            s =>s.FollowAsync(followerId, followeeId), Times.Once());
+    }
+
+
+}
