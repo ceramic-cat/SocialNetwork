a0388de7f353fae30f9e3aa728683fe749d39868
emmamjs-7 <emmaamjs@gmail.com>
2025-12-04 13:17:32 +0100

    moved to different folders

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { Button } from "react-bootstrap";
-import CreatePostModal from "../components/CreatePostModal";
+import CreatePostModal from "../modals/CreatePostModal";
 import AuthModal from "../modals/AuthModal";
 import useCurrentUser from "../hooks/useCurrentUser";
 
