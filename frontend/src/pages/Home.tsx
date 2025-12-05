886a8c56ead49dd2ff4abf5e83f0e4acf946dc03
emmamjs-7 <emmaamjs@gmail.com>
2025-12-05 11:03:34 +0100

    cleaned up home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,37 +1,3 @@
-import { Button } from "react-bootstrap";
-import CreatePostModal from "../modals/CreatePostModal";
-import AuthModal from "../modals/AuthModal";
-import useCurrentUser from "../hooks/useCurrentUser";
-
 export default function Home() {
-  const {
-    isLoggedIn,
-    loading,
-    userId,
-    username,
-    handleLoginSuccess,
-    handleLogout,
-  } = useCurrentUser();
-
-  if (loading) {
-    return <div className="text-center">Loading...</div>;
-  }
-
-  if (!isLoggedIn) {
-    return (
-      <AuthModal showInitially={true} onLoginSuccess={handleLoginSuccess} />
-    );
-  }
-
-  return (
-    <div className="text-center">
-      {userId && username && (
-        <CreatePostModal senderId={userId} senderName={username} />
-      )}
-
-      <Button variant="outline-danger" onClick={handleLogout}>
-        Logout
-      </Button>
-    </div>
-  );
+  return <div className="text-center"></div>;
 }
