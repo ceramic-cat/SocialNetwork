1f14d709b6c03182c11388bc3030e9658ab5daca
Angelica <angelicanorlander@telia.com>
2025-12-05 14:34:40 +0100

    Add post state and list rendering to Timeline component
    Store fetched posts in state
    Render simple list of post.content

--- frontend/src/pages/Timeline.tsx
+++ frontend/src/pages/Timeline.tsx
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
+import { Card } from "react-bootstrap";
+import { useState } from "react";
 
 type PostDto = {
   id: string;
   senderId: string;
-  reciverId: string;
+  recieverId: string;
   content: string;
   createdAt: string;
 };
@@ -11,19 +13,39 @@ type PostDto = {
 const BASE_URL = "http://localhost:5148";
 
 export default function Timeline() {
-  const userId = "This works with a hardcoded user id";
+  const userId = "Works if userid is added hardcoded";
+
+  const [posts, setPosts] = useState<PostDto[]>([]);
 
   useEffect(() => {
     fetch(`${BASE_URL}/api/users/${userId}/timeline`)
       .then((res) => res.json())
       .then((data: PostDto[]) => {
         console.log("Timeline data:", data);
+        setPosts(data);
       })
       .catch((err) => console.error("Failed to load timeline:", err));
   }, []);
+
   return (
     <div>
       Timeline Page - Under Construction. Try looking at the console.log
+      <Card className="mt-5">
+        <Card.Header>Quote</Card.Header>
+        <Card.Body>
+          <blockquote className="blockquote mb-0">
+            <ul>
+              {posts.map((post) => (
+                <li key={post.id}>{post.content}</li>
+              ))}
+            </ul>
+            <footer className="blockquote-footer">
+              Someone famous in <cite title="Source Title">Source Title</cite>
+            </footer>
+          </blockquote>
+        </Card.Body>
+      </Card>
+      ;
     </div>
   );
 }
