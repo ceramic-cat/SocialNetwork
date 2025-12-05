9fe0b2c7961141d65cea3d3c0d2ad2ecdbfd053e
emmamjs-7 <emmaamjs@gmail.com>
2025-12-05 11:07:29 +0100

    moved modal logic to App to make it global

--- frontend/src/modals/CreatePostModal.tsx
+++ frontend/src/modals/CreatePostModal.tsx
@@ -1,46 +1,33 @@
-import { useState } from "react";
-import { Button, Modal } from "react-bootstrap";
+import { Modal, Button } from "react-bootstrap";
 import CreatePostForm from "../forms/CreatePostForm";
 
 export interface CreatePostModalProps {
   senderId: string;
   senderName: string;
+  show: boolean;
+  onClose: () => void;
 }
 
 export default function CreatePostModal({
   senderId,
   senderName,
+  show,
+  onClose,
 }: CreatePostModalProps) {
-  const [show, setShow] = useState(false);
-
-  function handleOpen() {
-    setShow(true);
-  }
-
-  function handleClose() {
-    setShow(false);
-  }
-
   return (
-    <>
-      <Button variant="primary" onClick={handleOpen}>
-        Create a post
-      </Button>
-
-      <Modal show={show} onHide={handleClose} centered>
-        <Modal.Body>
-          <CreatePostForm
-            senderId={senderId}
-            senderName={senderName}
-            onSuccess={handleClose}
-          />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+    <Modal show={show} onHide={onClose} centered>
+      <Modal.Body>
+        <CreatePostForm
+          senderId={senderId}
+          senderName={senderName}
+          onSuccess={onClose}
+        />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onClose}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
