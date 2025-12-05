5deb82e5d10c7a7f3fcc0c7da47c1e145cbf6bb2
ceramic-cat <120321392+ceramic-cat@users.noreply.github.com>
2025-12-05 11:52:24 +0100

    Added changes to database seed and updated models with column names.

--- backend/SocialNetwork.Entity/Models/Post.cs
+++ backend/SocialNetwork.Entity/Models/Post.cs
@@ -1,11 +1,18 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace SocialNetwork.Entity.Models
 {
     public class Post
     {
-        public Guid Id { get; set; } = Guid.NewGuid();  
+        [Column("id")]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        [Column("senderId")]
         public Guid SenderId { get; set; }
+        [Column("receiverId")]
         public Guid ReceiverId { get; set; }
+        [Column("content")]
         public string Content { get; set; } = string.Empty;
+        [Column("createdAt")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow; 
     }
 }
