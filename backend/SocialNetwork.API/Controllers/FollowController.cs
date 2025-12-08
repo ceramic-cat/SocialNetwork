9284b3bb2244db9cec0f7ec977b2fad806553508
ceramic-cat <120321392+ceramic-cat@users.noreply.github.com>
2025-12-08 10:33:19 +0100

    Added xml comments documenting FollowingController for SwaggerUI.

--- backend/SocialNetwork.API/Controllers/FollowController.cs
+++ backend/SocialNetwork.API/Controllers/FollowController.cs
@@ -15,7 +15,9 @@ public interface IFollowController
     Task<IActionResult> IsFollowingAsync(Guid followeeId);
     Task<IActionResult> UnfollowAsync(Guid followeeId);
 }
-
+/// <summary>
+/// Handles users following other users and related information.
+/// </summary>
 [Route("api/[controller]")]
 [ApiController]
 public class FollowController : ControllerBase, IFollowController
@@ -26,6 +28,11 @@ public class FollowController : ControllerBase, IFollowController
     {
         _followService = followsService;
     }
+
+    /// <summary>
+    /// Adds a follower-follwee relationship to logged in user.
+    /// </summary>
+    /// <param name="followeeId">User guid to follow</param>
     [Authorize]
     [HttpPost(":followeeId")]
     public async Task<IActionResult> FollowAsync(Guid followeeId)
@@ -48,6 +55,10 @@ public class FollowController : ControllerBase, IFollowController
         return BadRequest(result.ErrorMessage);
 
     }
+    /// <summary>
+    /// Logged in user unfollows followee.
+    /// </summary>
+    /// <param name="followeeId">User guid to unfollow</param>
     [Authorize]
     [HttpDelete(":followeeId")]
     public async Task<IActionResult> UnfollowAsync(Guid followeeId)
@@ -66,6 +77,10 @@ public class FollowController : ControllerBase, IFollowController
         return BadRequest(result.ErrorMessage);
     }
 
+    /// <summary>
+    /// Get all users logged in user follows.
+    /// </summary>
+    /// <returns>Returns array of guid. If no follows returns empty array.</returns>
     [Authorize]
     [HttpGet]
     public async Task<IActionResult> GetFollowsAsync()
@@ -86,6 +101,11 @@ public class FollowController : ControllerBase, IFollowController
 
         return BadRequest(result.ErrorMessage);
     }
+    /// <summary>
+    /// Check if there is a following relationship between logged in user and followeeId.
+    /// </summary>
+    /// <param name="followeeId">User id to check if logged in user follows.</param>
+    /// <returns>Returns boolean</returns>
     [Authorize]
     [HttpGet(":followeeId")]
     public async Task<IActionResult> IsFollowingAsync(Guid followeeId)
