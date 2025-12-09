b092c92d504810b7df0b45366893bb2dc91b386a
ceramic-cat <120321392+ceramic-cat@users.noreply.github.com>
2025-12-09 16:27:08 +0100

    Removed unused props and adjusted header margin

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -2,14 +2,14 @@
 import { Col, Container, Row } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import comet from "../assets/comet.svg"
-type Props = {}
 
-export default function NotFound({ }: Props) {
+export default function NotFound() {
   return (
     <Container fluid className="p-5 text-primary min-vh-100 max-vw-80"  >
       <Row className="justify-content-center align-items-center g-5">
         <Col xs={10} sm={6} md={6} lg={5}>
-          <h1>Page Not found</h1>
+          <h1 className="my-3 my-md-5 ">Page Not found</h1>
+
           <p>Seems you've gotten lost among the stars...</p>
           <Link to={('/')}>Return to homebase</Link>
         </Col>
